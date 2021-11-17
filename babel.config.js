0bf23f8d6eb2f23605d6b77b81d8747e0cf84501
Dale <1175204449@qq.com>
2021-11-17 21:33:19 +0800

    chore: 添加elementUI和CustomTree

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,19 @@
+/*
+ * @Author: Dalegac
+ * @Date: 2021-11-17 17:11:11
+ * @LastEditTime: 2021-11-17 17:35:09
+ * @LastEditors: Dalegac
+ * @Description: Just say something
+ */
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+  presets: ["@vue/cli-plugin-babel/preset", "@vue/babel-preset-jsx"],
+  plugins: [
+    [
+      "component",
+      {
+        libraryName: "element-ui",
+        styleLibraryName: "theme-chalk",
+      },
+    ],
+  ],
+};
---
+++ src/plugins/element.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: Dalegac
+ * @Date: 2021-11-17 17:19:47
+ * @LastEditTime: 2021-11-17 21:04:22
+ * @LastEditors: Dalegac
+ * @Description: Just say something
+ */
+import Vue from "vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+Vue.use(ElementUI);
